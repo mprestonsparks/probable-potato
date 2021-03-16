b7f4e823a91617d1751da84b6e0975d79f245a33
Preston <mprestonsparks@gmail.com>
2021-03-15 21:32:17 -0500

    added callback example

--- coingecko.js
+++ coingecko.js
@@ -1,26 +1,69 @@
 
+// call functions
+getData();
+
+
+// declare array of tokens to return data for
 var coin = [
-    {"id":"bitcoin"},
-    {"id":"ethereum"},    
+    "bitcoin",
+    "ethereum",
 ];
 
-getData();
-// get data from Coingecko API
-async function getData() {
-        for (var i = 0; i < coin.length; i++) {
-        
-        // const api_url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin';
-        const api_url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin';
-        const response = await fetch(api_url);
-        const data = await response.json();
-        console.log(coin[0].id)
-        console.log(data);
 
-        for (var i = 0; i < data.length; i++) {
-            tr = $('<tr/>');
-            tr.append("<td>" + data[i].symbol + "</td>");
-            tr.append("<td>" + data[i].ath + "</td>");
-            $('table').append(tr);
-        };
+// -------------------------------------------------
+// ** WRAP THE WHOLE FUNCTION IN THIS CALLBACK
+=const slowAndSteady = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        console.log('Slow and steady wins the race.');
+        resolve();
+    }, 5000);
+});
+slowAndSteady
+    .then(function() {
+        console.log('The last shall be the first!');
+    });
+// -------------------------------------------------
+
+
+// get data from Coingecko API
+async function getData(coin) { 
+    
+    // ** FIX ** 
+    // the getData() function only works when the coin array is defined within the function
+    // get it to work with the global coin array
+    var coin = [
+        "bitcoin",
+        "ethereum",
+    ];
+
+    for (var i = 0; i < coin.length; i++) {
+        // const api_url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin';
+        const api_url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=' + coin[i];
+        const response = await fetch(api_url);
+        // response from API call is stored as 'data'
+        const data = await response.json();
+        console.log(data);
+        updateTable(data);
+
+    };
+
+};
+
+// add JSON data to table HTML
+function updateTable(data) {
+
+    // ** FIX ** 
+    // the getData() function only works when the coin array is defined within the function
+    // get it to work with the global coin array
+    var coin = [
+        "bitcoin",
+        "ethereum",
+    ];
+
+    for (var i = 0; i < coin.length; i++) {
+        tr = $('<tr/>');
+        tr.append("<td>" + data[i].symbol + "</td>");
+        tr.append("<td>" + data[i].current_price + "</td>");
+        $('table').append(tr);
     };
 };
